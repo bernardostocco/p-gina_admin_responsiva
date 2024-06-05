6ddf35a11e450160c04f82c0ddcbcf38f2b8bff3
Bernardo Whehaibe <bernardostocco12@gmail.com>
2024-06-05 09:05:05 -0300

    Admin responsiva

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Curso exemplo 1',
+        productNumber: '124721',
+        paymentStatus: 'Aguardando',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Curso exemplo 2',
+        productNumber: '666666',
+        paymentStatus: 'Reembolsado',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Curso exemplo 3',
+        productNumber: '846234',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
